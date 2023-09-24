68458bff01145bc5d0364d40f31792858bcd954e
bettayub <bettayub19@gmail.com>
2023-09-24 20:46:21 +0300

    creating database to interact with SQLAlchemy

--- app.py
+++ app.py
@@ -3,3 +3,8 @@
 
 #creating instance
 app = Flask(__name__)
+
+
+#creating database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///emp.db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'FALSE'
