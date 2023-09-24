a63d29909f7d954e5810341c53c4eba1debea8ba
bettayub <bettayub19@gmail.com>
2023-09-24 20:49:49 +0300

    Initialize SQLAlchemy with 'app' for database connection

--- app.py
+++ app.py
@@ -8,3 +8,6 @@ app = Flask(__name__)
 #creating database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///emp.db'
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'FALSE'
+
+# Initialize SQLAlchemy with 'app' for database connection.
+db = SQLAlchemy(app)
