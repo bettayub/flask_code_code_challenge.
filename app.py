50b32b217982a247f138ddfeefdb505f2e75c333
bettayub <bettayub19@gmail.com>
2023-09-24 20:51:35 +0300

    Adding classe pizzas

--- app.py
+++ app.py
@@ -11,3 +11,15 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///emp.db'
 
 # Initialize SQLAlchemy with 'app' for database connection.
 db = SQLAlchemy(app)
+
+
+#Adding classe piizzas
+class Pizzas(db.Model):
+    id = db.Column(db.Integer,primary_key =True)
+    name = db.Column(db.String(255),nullable=False)
+    ingredients = db.Column(db.String(200), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow())
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow())
+                            
+       
+
