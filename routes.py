13061421f1b8b296601e9e294487b0e01f68eb8c
bettayub <bettayub19@gmail.com>
2023-09-25 20:35:45 +0300

    routing function restaurants

--- routes.py
+++ routes.py
@@ -7,3 +7,19 @@ from app import app
 # Setting the SQLAlchemy database Uniform Resource Identifier
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///emp.db'
 db = SQLAlchemy(app)
+
+
+# model routes function for Restaurants 
+
+@app.route('/restaurants', methods=['GET'])
+def get_restaurants():
+    restaurants = Restaurant.query.all()
+    restaurant_list = []
+    for restaurant in restaurants:
+        restaurant_data = {
+            'id': restaurant.id,
+            'name': restaurant.name,
+            'address': restaurant.address
+        }
+        restaurant_list.append(restaurant_data)
+    return jsonify(restaurant_list)
